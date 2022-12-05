4dfeb05585973a89bee59fd81d7d6af1e0bb66c8
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-12-04 21:16:57 -0800

    group rotation

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -1,40 +1,59 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from 'three';
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
-// Scene
-const scene = new THREE.Scene()
-
-/**
- * Objects
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-/**
- * Sizes
- */
+//NOTE =========================================================================== Sizes 
 const sizes = {
-    width: 800,
-    height: 600
-}
+    width: 800, 
+    height: 600, 
+};
+const aspectRatio = sizes.width / sizes.height;
+const scene = new THREE.Scene();
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const group = new THREE.Group();
+scene.add(group);
 
-/**
- * Renderer
- */
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: "#bde0fe"})
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: "#ffc8dd"}),
+)
+
+cube2.position.set(1, 0, 0);
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: "#cdb4db"}), 
+)
+
+cube3.position.set(-1, 0, 0);
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+group.rotation.x = 2.3;
+group.rotation.y = 0.5;
+
+//NOTE =========================================================================== Axes helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+//NOTE =========================================================================== Camera 
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+camera.position.set(0, 0, 3);
+scene.add(camera);
+
+//NOTE =========================================================================== Renderer 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas, 
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+renderer.setClearColor("#023047");
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
