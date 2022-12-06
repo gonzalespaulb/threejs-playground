781e5efa71b65a75bc7e5dd3461555631a58ccf2
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-12-06 15:08:42 -0800

    made a floor

---
+++ playground/src/script.js
@@ -0,0 +1,86 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// Cursor Coordinates
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const aspectRatio = sizes.width / sizes.height;
+
+const scene = new THREE.Scene();
+const axesHelper = new THREE.AxesHelper(3);
+const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+const renderer = new THREE.WebGL1Renderer({
+  canvas, 
+})
+
+
+// NOTE ---------------------------------------------------------------- BASE 1
+const base = new THREE.Group();
+
+const block1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#e76f51"})
+)
+
+const block2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#f4a261"})
+)
+
+const block3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#f4a261"})
+)
+
+block2.position.set(1, 0, 0);
+block3.position.set(-1, 0, 0);
+
+base.add(block1, block2, block3);
+
+
+// NOTE ---------------------------------------------------------------- BASE 2
+
+const block4 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#e76f51"})
+);
+block4.position.set(-1, 0, -1);
+
+const block5 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#f4a261"})
+);
+block5.position.set(0, 0, -1);
+
+const block6 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.MeshBasicMaterial({color: "#e76f51"})
+)
+block6.position.set(1, 0, -1);
+
+base.add(block4, block5, block6);
+
+
+
+scene.add(base);
+scene.add(axesHelper);
+
+camera.position.set(0, 3, 3);
+
+camera.lookAt(base.position);
+scene.add(camera);
+
+renderer.setClearColor("#264653");
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
