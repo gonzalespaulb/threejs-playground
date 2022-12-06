32ad5f5e4399b012dcbf4cd82d58098e6a399b6d
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-12-06 15:27:08 -0800

    made a floor

--- playground/src/script.js
+++ playground/src/script.js
@@ -29,17 +29,17 @@ const renderer = new THREE.WebGL1Renderer({
 const base = new THREE.Group();
 
 const block1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#e76f51"})
 )
 
 const block2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#f4a261"})
 )
 
 const block3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#f4a261"})
 )
 
@@ -52,35 +52,46 @@ base.add(block1, block2, block3);
 // NOTE ---------------------------------------------------------------- BASE 2
 
 const block4 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#e76f51"})
 );
 block4.position.set(-1, 0, -1);
 
 const block5 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#f4a261"})
 );
 block5.position.set(0, 0, -1);
 
 const block6 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1), 
+  new THREE.BoxGeometry(1, 0.5, 1), 
   new THREE.MeshBasicMaterial({color: "#e76f51"})
 )
 block6.position.set(1, 0, -1);
 
 base.add(block4, block5, block6);
 
-
-
 scene.add(base);
 scene.add(axesHelper);
 
 camera.position.set(0, 3, 3);
 
-camera.lookAt(base.position);
+// camera.lookAt(base.position);
+
 scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 renderer.setClearColor("#264653");
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+// NOTE ---------------------------------------------------------------- CONTROL ORBIT BY DRAGGING THE MOUSE
+const tick = () => {
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+}
+
+tick();
