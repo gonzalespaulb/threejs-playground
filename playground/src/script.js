a06814676d45e67af907db31faac6b100abd02e0
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-12-08 17:57:29 -0800

    created seat

--- playground/src/script.js
+++ playground/src/script.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import GUI from "lil-gui";
 // Cursor Coordinates
 
 /**
@@ -8,6 +10,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
  */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
+const gui = new GUI({ width: 400 });
 
 // Sizes
 const sizes = {
@@ -15,83 +18,59 @@ const sizes = {
   height: window.innerHeight,
 };
 
+const colors = {
+  base: "#99582a",
+};
+
 const aspectRatio = sizes.width / sizes.height;
 
 const scene = new THREE.Scene();
 const axesHelper = new THREE.AxesHelper(3);
-const camera = new THREE.PerspectiveCamera(75, aspectRatio)
+const camera = new THREE.PerspectiveCamera(75, aspectRatio);
 const renderer = new THREE.WebGL1Renderer({
-  canvas, 
-})
+  canvas,
+});
 
+// NOTE ---------------------------------------------------------------- Chair group
+const chair = new THREE.Group();
 
-// NOTE ---------------------------------------------------------------- BASE 1
-const base = new THREE.Group();
-
-const block1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#e76f51"})
-)
-
-const block2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#f4a261"})
-)
-
-const block3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#f4a261"})
-)
-
-block2.position.set(1, 0, 0);
-block3.position.set(-1, 0, 0);
-
-base.add(block1, block2, block3);
-
-
-// NOTE ---------------------------------------------------------------- BASE 2
-
-const block4 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#e76f51"})
+// NOTE ---------------------------------------------------------------- Seat
+const seat = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({
+    color: colors.base,
+  })
 );
-block4.position.set(-1, 0, -1);
+chair.add(seat);
 
-const block5 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#f4a261"})
-);
-block5.position.set(0, 0, -1);
+const chairSeat = gui.addFolder("Seat");
+const seatScaleX = chairSeat.add(seat.scale, "x");
+seatScaleX.min(0).max(10).step(0.01).name("x-scale");
 
-const block6 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 0.5, 1), 
-  new THREE.MeshBasicMaterial({color: "#e76f51"})
-)
-block6.position.set(1, 0, -1);
+const seatScaleY = chairSeat.add(seat.scale, "y");
+seatScaleY.min(0).max(10).step(0.01).name("y-scale");
 
-base.add(block4, block5, block6);
-
-scene.add(base);
+const seatScaleZ = chairSeat.add(seat.scale, "z");
+seatScaleZ.min(0).max(10).step(0.01).name("z-scale");
+// NOTE ---------------------------------------------------------------- Add to scene
+scene.add(chair);
 scene.add(axesHelper);
 
-camera.position.set(0, 3, 3);
-
-// camera.lookAt(base.position);
+camera.position.set(0, 0, 3);
 
 scene.add(camera);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-renderer.setClearColor("#264653");
+renderer.setClearColor("#dad7cd");
 renderer.setSize(sizes.width, sizes.height);
 
 // NOTE ---------------------------------------------------------------- CONTROL ORBIT BY DRAGGING THE MOUSE
 const tick = () => {
-
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
-}
+};
 
 tick();
