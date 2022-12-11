b8334cfa1d847535c5942a979e6b2b88f2d8ae91
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-12-10 17:22:56 -0800

    long ass lesson

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -6,9 +6,51 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  * Base
  */
 
+const loadingManager = new THREE.LoadingManager();
+
+// loadingManager.onStart = () => {
+//     console.log('just started')
+// };
+
+// loadingManager.onLoad = () => {
+//     console.log('loaded')
+// };
+
+// loadingManager.onProgress = () => {
+//     console.log('loading')
+// };
+
+// loadingManager.onError = () => {
+//     console.log('u fucked up somewhere')
+// };
+
 // Textures
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/textures/door/color.jpg')
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load('/textures/minecraft.png');
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const heightTexture = textureLoader.load('/textures/door/height.jpg');
+const normalTexture = textureLoader.load('/textures/door/normal.jpg');
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
+// colorTexture.repeat.x = 2;
+// colorTexture.repeat.y = 3;
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5;
+// colorTexture.offset.y = 0.5;
+
+// colorTexture.rotation = 1;
+
+// colorTexture.rotation = Math.PI / 4;
+// colorTexture.center.x = 0.5;
+// colorTexture.center.y = 0.5;
+
+colorTexture.generateMipmaps = false;
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -20,8 +62,11 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: texture })
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+console.log(geometry.attributes.uv);
+
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -53,9 +98,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 1
+// camera.position.x = 1
+// camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
